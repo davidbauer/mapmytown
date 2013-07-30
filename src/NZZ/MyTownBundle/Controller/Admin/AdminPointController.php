9007529ce48a88dbf6f570b98276a4fb24c87aef
volodymyr.fertak <volodymyr.fertak@nzz.ch>
2013-07-30 22:31:40 +0300

    added pagination

--- src/NZZ/MyTownBundle/Controller/Admin/AdminPointController.php
+++ src/NZZ/MyTownBundle/Controller/Admin/AdminPointController.php
@@ -17,10 +17,19 @@ class AdminPointController extends Controller
 {
     public function indexAction($projectId)
     {
+        $limit = 1;
+        $request = $this->getRequest()->query;
+        $offset = ($request->get('offset')) ? $request->get('offset') : 0 ;
+        $count = PointQuery::create()
+            ->joinProject()
+            ->filterByProjectid($projectId)
+            ->find()->count();
         $points  = PointQuery::create()
             ->joinProject()
             ->filterByProjectid($projectId)
+            ->offset($offset)
             ->orderById(Criteria::DESC)
+            ->limit($limit)
             ->find()->toArray(null,false,BasePeer::TYPE_FIELDNAME);
         $fields = array(
             'id',
@@ -32,74 +41,11 @@ class AdminPointController extends Controller
         );
 
         return $this->render('NZZMyTownBundle:Admin\Point:index.html.twig', array(
+                'count' => $count,
+                'limit' => $limit,
                 'fields' => $fields,
-                'points' => $points
-            )
-        );
-    }
-
-    public function addAction()
-    {
-//        if (false === $this->get('security.context')->isGranted('ROLE_ADMIN')) {
-//            return $this->redirect($this->generateUrl('vfe_emotion_homepage'));
-//        }
-        $project = new Project();
-        $form = $this->createFormBuilder($project)
-            ->add('name', 'text', array('required' => true))
-            ->add('shortname','text', array('required' => true))
-            ->add('centerlatitude','text', array('required' => true))
-            ->add('centerlatitude', 'text', array('required' => true))
-            ->add('centerlongitude', 'text', array('required' => true))
-            ->add('defaultzoom', 'text', array('required' => true))
-            ->add('language', 'choice', array('required' => true,
-                    'choices'   => array('en' => 'English', 'fr' => 'French', 'de' => 'Deutsch')))
-            ->add('save', 'submit')
-            ->getForm();
-
-        return $this->render('NZZMyTownBundle:Admin:add.html.twig', array(
-                'form' => $form->createView()
-            )
-        );
-    }
-
-    public function saveAction()
-    {
-        $data = $this->getRequest()->get('form');
-        if (!empty($data['id'])) {
-            $project = ProjectQuery::create()->findOneById($data['id']);
-        } else {
-            $project  = new Project();
-        }
-        $project->setName($data['name']);
-        $project->setShortname($data['shortname']);
-        $project->setCenterlatitude($data['centerlatitude']);
-        $project->setCenterlongitude($data['centerlongitude']);
-        $project->setDefaultzoom($data['defaultzoom']);
-        $project->setLanguage($data['language']);
-        $project->save();
-
-        return $this->redirect($this->generateUrl('nzz_my_town_admin_dashboard'));
-    }
-
-
-    public function editProjectAction($projectId)
-    {
-        $project = ProjectQuery::create()->findOneById($projectId);
-        $form = $this->createFormBuilder($project)
-            ->add('id','text',array('read_only' => true))
-            ->add('name', 'text', array('required' => true))
-            ->add('shortname', 'text', array('required' => true))
-            ->add('centerlatitude', 'text', array('required' => true))
-            ->add('centerlatitude', 'text', array('required' => true))
-            ->add('centerlongitude', 'text', array('required' => true))
-            ->add('defaultzoom', 'text', array('required' => true))
-            ->add('language', 'choice', array('required' => true,
-                    'choices'   => array('en' => 'English', 'fr' => 'French', 'de' => 'Deutsch')))
-            ->add('save', 'submit')
-            ->getForm();
-
-        return $this->render('NZZMyTownBundle:Admin:editProject.html.twig', array(
-                'form' => $form->createView()
+                'points' => $points,
+                'projectId' => $projectId
             )
         );
     }
