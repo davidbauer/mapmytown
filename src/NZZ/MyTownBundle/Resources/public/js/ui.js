c81020254e61cfe6564d0956f03bbca0398face9
Benjamin Wiederkehr <me@benjaminwiederkehr.com>
2013-07-31 16:09:52 +0200

    Massive design update.

--- src/NZZ/MyTownBundle/Resources/public/js/ui.js
+++ src/NZZ/MyTownBundle/Resources/public/js/ui.js
@@ -9,4 +9,21 @@ $( document ).ready(function() {
   $('.embed_popover').click(function(){
     $('#embedding_code').select();
   })
+  /*
+  $('.submit__marker__section').hide();
+
+  $('#submit__wisdom__btn').click(function(){
+    $('.comments__section').hide();
+    $('.submit__wisdom__section').hide();
+  })
+  $('#cancel__marker__btn').click(function(){
+    $('.comments__section').show();
+    $('.submit__comment__section').hide();
+    $('.submit__marker__section').show();
+  })
+  $('#submit__marker__btn').click(function(){
+    $('.submit__marker__section').hide();
+  })
+  */
+
 });
