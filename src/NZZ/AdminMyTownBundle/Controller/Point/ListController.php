d397e8b7d6227a6b61f5c37027d46d456a90050c
volodymyr.fertak <volodymyr.fertak@nzz.ch>
2013-08-01 02:31:43 +0300

    revert

--- src/NZZ/AdminMyTownBundle/Controller/Point/ListController.php
+++ src/NZZ/AdminMyTownBundle/Controller/Point/ListController.php
@@ -7,33 +7,79 @@ use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class ListController extends BaseListController
 {
-    public function filtersAction()
+    public function indexAction()
     {
-        if ($this->get('request')->get('reset')) {
-            $this->setFilters(array());
 
-            return new RedirectResponse($this->generateUrl("NZZ_AdminMyTownBundle_Point_list"));
-        }
+        $this->parseRequestForPager();
 
-        if ($this->getRequest()->getMethod() == "POST") {
-            $form = $this->getFilterForm();
-            $form->bind($this->get('request'));
+        $form = $this->getFilterForm();
 
-            if ($form->isValid()) {
-                $filters = $form->getViewData();
-            }
-        }
-
-        if ($this->getRequest()->getMethod() == "GET") {
-            $filters = $this->getRequest()->query->all();
-        }
-
-        $filters['project_id'] = 2;
-        if (isset($filters)) {
-            $this->setFilters($filters);
-        }
-
-        return new RedirectResponse($this->generateUrl("NZZ_AdminMyTownBundle_Point_list"));
+        return $this->render('NZZAdminMyTownBundle:PointList:index.html.twig', $this->getAdditionalRenderParameters() + array(
+                'Points' => $this->getPager(),
+                'form'                      => $form->createView(),
+                'sortColumn'                => $this->getSortColumn(),
+                'sortOrder'                 => $this->getSortOrder(),
+                'scopes'                    => $this->getScopes(),
+            ));
     }
 
+    protected function processFilters($query)
+    {
+        if ($this->getRequest()->getMethod() == "GET") {
+            $filterProjectId = $this->getRequest()->query->get('projectId');
+        }
+        if (isset($filterProjectId) && null !== $filterProjectId) {
+            $fl = $this->getFilters();
+                $fl['project_id'] = $filterProjectId;
+                $this->setFilters($fl);
+        }
+
+        $filterObject = $this->getFilters();
+
+        $queryFilter = $this->get('admingenerator.queryfilter.propel');
+        $queryFilter->setQuery($query);
+
+        if (isset($filterObject['project_id']) && null !== $filterObject['project_id']) {
+            $queryFilter->addIntegerFilter("project_id", $filterObject['project_id']);
+        }
+
+        if (isset($filterObject['id']) && null !== $filterObject['id']) {
+            $queryFilter->addIntegerFilter("id", $filterObject['id']);
+        }
+
+        if (isset($filterObject['title']) && null !== $filterObject['title']) {
+            $queryFilter->addVarcharFilter("title", $filterObject['title']);
+        }
+
+        if (isset($filterObject['description']) && null !== $filterObject['description']) {
+            $queryFilter->addVarcharFilter("description", $filterObject['description']);
+        }
+
+        if (isset($filterObject['latitude']) && null !== $filterObject['latitude']) {
+            $queryFilter->addFloatFilter("latitude", $filterObject['latitude']);
+        }
+
+        if (isset($filterObject['longitude']) && null !== $filterObject['longitude']) {
+            $queryFilter->addFloatFilter("longitude", $filterObject['longitude']);
+        }
+
+        if (isset($filterObject['author_name']) && null !== $filterObject['author_name']) {
+            $queryFilter->addVarcharFilter("author_name", $filterObject['author_name']);
+        }
+
+        if (isset($filterObject['author_location']) && null !== $filterObject['author_location']) {
+            $queryFilter->addVarcharFilter("author_location", $filterObject['author_location']);
+        }
+
+        if (isset($filterObject['sentiment']) && null !== $filterObject['sentiment']) {
+            $queryFilter->addIntegerFilter("sentiment", $filterObject['sentiment']);
+        }
+
+        if (isset($filterObject['is_published']) && null !== $filterObject['is_published']) {
+            $queryFilter->addBooleanFilter("is_published", $filterObject['is_published']);
+        }
+
+
+
+    }
 }
