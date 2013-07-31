eadcb574e3cee4d31ab4d4095553a1f1ab790479
Peter Gassner <peter@naehrstoff.ch>
2013-07-31 12:37:31 +0200

    Added i18n support to FE

---
+++ src/NZZ/MyTownBundle/Resources/public/js/utils/HandlebarsHelpers.js
@@ -0,0 +1,25 @@
+/**
+ * Usage:
+ *    span {{t "my.key" }}
+ */
+Handlebars.registerHelper('t', function(i18n_key) {
+  var result = i18n.t(i18n_key);
+ 
+  return new Handlebars.SafeString(result);
+});
+
+/**
+ * Usage:
+ *     {{#tr this key="trans.sample.handlebarsExtended" add="from helper" }}
+ *     h6 Some Text
+ *     p some paragraph with variable __add__ __addFromContext__
+ *     {/tr}
+ */
+Handlebars.registerHelper('tr', function(context, options) { 
+   var opts = i18n.functions.extend(options.hash, context);
+   if (options.fn) opts.defaultValue = options.fn(context);
+ 
+   var result = i18n.t(opts.key, opts);
+ 
+   return new Handlebars.SafeString(result);
+});
