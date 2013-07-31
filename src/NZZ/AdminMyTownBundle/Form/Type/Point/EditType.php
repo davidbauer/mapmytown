3bf0118b3b657c683cd78dcf04ca43b91ed6186b
volodymyr.fertak <volodymyr.fertak@nzz.ch>
2013-08-01 00:15:26 +0300

    point editing

---
+++ src/NZZ/AdminMyTownBundle/Form/Type/Point/EditType.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace NZZ\AdminMyTownBundle\Form\Type\Point;
+
+use Admingenerated\NZZAdminMyTownBundle\Form\BasePointType\EditType as BaseEditType;
+
+class EditType extends BaseEditType
+{
+}
---
+++ src/NZZ/AdminMyTownBundle/Form/Type/Point/NewType.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace NZZ\AdminMyTownBundle\Form\Type\Point;
+
+use Admingenerated\NZZAdminMyTownBundle\Form\BasePointType\NewType as BaseNewType;
+
+class NewType extends BaseNewType
+{
+}
