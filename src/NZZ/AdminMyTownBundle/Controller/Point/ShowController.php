2b70bc6c8b1bedf8f743972fb40c4f817295912d
volodymyr.fertak <volodymyr.fertak@nzz.ch>
2013-08-01 01:35:50 +0300

    added points

--- src/NZZ/AdminMyTownBundle/Controller/Point/ShowController.php
+++ src/NZZ/AdminMyTownBundle/Controller/Point/ShowController.php
@@ -6,4 +6,19 @@ use Admingenerated\NZZAdminMyTownBundle\BasePointController\ShowController as Ba
 
 class ShowController extends BaseShowController
 {
+    public function indexAction($pk)
+    {
+     var_dump('ololo');die;
+        $Point = $this->getObject($pk);
+
+
+
+        if (!$Point) {
+            throw new NotFoundHttpException("The NZZ\MyTownBundle\Model\Point with Id $pk can't be found");
+        }
+
+        return $this->render('NZZAdminMyTownBundle:PointShow:index.html.twig', $this->getAdditionalRenderParameters($Point) + array(
+                "Point" => $Point
+            ));
+    }
 }
--- src/NZZ/MyTownBundle/Model/Point.php
+++ src/NZZ/MyTownBundle/Model/Point.php
@@ -2,8 +2,17 @@
 
 namespace NZZ\MyTownBundle\Model;
 
+use NZZ\MyTownBundle\Model\Project;
+use NZZ\MyTownBundle\Model\ProjectQuery;
 use NZZ\MyTownBundle\Model\om\BasePoint;
 
 class Point extends BasePoint
 {
+    public function getProjectSlug()
+    {
+        /** @var Project $project */
+        $project = ProjectQuery::create()->findOneById($this->project_id);
+
+        return $project->getSlug();
+    }
 }
