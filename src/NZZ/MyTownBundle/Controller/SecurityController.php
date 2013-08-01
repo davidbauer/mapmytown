a225329f7efc090e43f508c047a2522abc1f54ef
volodymyr.fertak <volodymyr.fertak@nzz.ch>
2013-08-01 21:10:52 +0300

    redirect to new adminka

--- src/NZZ/MyTownBundle/Controller/SecurityController.php
+++ src/NZZ/MyTownBundle/Controller/SecurityController.php
@@ -4,6 +4,7 @@ namespace NZZ\MyTownBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Security\Core\SecurityContext;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class SecurityController extends Controller
 {
@@ -19,11 +20,12 @@ class SecurityController extends Controller
             $error = $session->get(SecurityContext::AUTHENTICATION_ERROR);
             $session->remove(SecurityContext::AUTHENTICATION_ERROR);
         }
-
-        return $this->render('NZZMyTownBundle::Admin\login.html.twig', array(
-                // last username entered by the user
-                'last_username' => $session->get(SecurityContext::LAST_USERNAME),
-                'error'         => $error,
-            ));
+        
+        return new RedirectResponse($this->generateUrl("_welcome" ));
+//        return $this->render('NZZMyTownBundle::Admin\login.html.twig', array(
+//                 last username entered by the user
+//                'last_username' => $session->get(SecurityContext::LAST_USERNAME),
+//                'error'         => $error,
+//            ));
     }
 }
